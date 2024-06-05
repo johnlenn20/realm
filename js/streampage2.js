c67a828430a3493b62c9dfdcc24dc03aa3da2d7a
johnlenn20 <johnlenong2020@gmail.com>
2024-06-05 22:17:24 +0700

    Update streampage3.js

--- js/streampage2.js
+++ js/streampage2.js
@@ -127,14 +127,6 @@ function anonAd() {
 	document.getElementById('ad-container').append(centerElement);
 }
 
-function dedyS() {
-	iframeElement.src = `https://nuju.my.id/mylink`;
-	iframeElement.width = `100%`;
-	iframeElement.height = `844`;
-	centerElement.appendChild(iframeElement);
-	document.getElementById('ad-container').append(centerElement);
-}
-
 function CewekDesa14() {
 	var values = ["www.highcpmgate.com/t2vzehcp?key=5b1d0ece292232c15a8fe8b47b6f8e84", "www.highcpmgate.com/zuijf9rng?key=4cac7aab30f933cfc28f97592409b803", "www.highcpmgate.com/g27ihyspv?key=fb67a42213443d7fb1e2663255f4c2d3", "www.highcpmgate.com/tweia98239?key=17497073aba14beb507ceaba3b4bb7a7", "chesssorrydescend.com/c6zx1ccmsb?key=a41b551f15fefdef584aa0291c62f7bc", "clipperroutesevere.com/tv1bm5gj?key=7bacd8dea51bef73c972734fb437ea22"];
 
@@ -199,9 +191,6 @@ if (num == 10) {
 		CewekDesa14();
 	}, 10000);
 	anonAd();
-	setTimeout(() => {
-		dedyS();
-	}, 240000);
 	var thsnow = new Date(),
 		thscurrentHour = thsnow.getHours();
 	if (thscurrentHour >= 0 && thscurrentHour <= 19) {} else if (thscurrentHour >= 20 && thscurrentHour <= 23) {}
